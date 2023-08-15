405d2a63ddfc5dcbaefa8382a498eade84b30382
renan horta <renan.horta96@gmail.com>
2023-08-15 15:48:33 -0300

    layout project finalized

---
+++ utils/resize_image.py
@@ -0,0 +1,15 @@
+#save button
+from PIL import Image
+
+def resize_image(image, maxsize):
+    r1 = image.size[0]/maxsize[0] # width ratio
+    r2 = image.size[1]/maxsize[1] # height ratio
+    ratio = max(r1, r2)
+    newsize = (int(image.size[0]/ratio), int(image.size[1]/ratio))
+    image = image.resize(newsize, Image.ANTIALIAS)
+    return image
+
+def resize_image_predifined(image, size):
+    newsize = (int(size[0]), int(size[1]))
+    image = image.resize(newsize, Image.ANTIALIAS)
+    return image
--- view_imageProcessor.py
+++ view_imageProcessor.py
@@ -13,6 +13,7 @@ root.overrideredirect(False)
 import tkinter as tk
 from tkinter import ttk
 from PIL import Image, ImageTk
+from utils.resize_image import resize_image, resize_image_predifined
 
 #from tkinter import ttk
 import customtkinter
@@ -24,8 +25,9 @@ root.overrideredirect(False)
 
 #colors in app
 pry_color = "#5376D6"
-sec_color = "#D9D9D9"
+sec_color = "#CFCECE"
 btn_color = "#6484DA"
+bg_container_color = "#DEDDDD"
 
 #font
 font='Monstserrat'
@@ -43,63 +45,85 @@ root.rowconfigure(0, weight=0)
 root.rowconfigure(1, weight=9)
 root.rowconfigure(2, weight=1)
 
+###############
 #top frame
+###############
+
 header_frame = tk.Frame(root, background = pry_color)
 header_frame.columnconfigure(0, weight=0)
 header_frame.columnconfigure(1, weight=8)
 header_frame.columnconfigure(2, weight=1)
 
 #home button
-img_home = tk.PhotoImage(file=r'src\images\logo_grey.png')
-img_home_label = tk.Label(image=img_home, background= pry_color)
-img_home_btn = tk.Button(header_frame, image= img_home,background= btn_color, borderwidth=0, activebackground=pry_color,relief='sunken')
+img_home= (Image.open(r'src\images\logo_grey.png'))
+btn_home_img = resize_image(img_home,(240,100))
+btn_home_model = ImageTk.PhotoImage(btn_home_img)
+home_btn = tk.Button(header_frame, image= btn_home_model, background= btn_color, borderwidth=0, activebackground=pry_color,relief='sunken')
 
 #upload button
-img_upload = tk.PhotoImage(file=r'src\images\upld_btn.png')
-img_upload_label = tk.Label(image=img_upload, background= pry_color)
-upload_btn = tk.Button(header_frame, image= img_upload, background = pry_color, borderwidth=0, activebackground=pry_color)
+
+img_upload= (Image.open(r'src\images\upld_btn.png'))
+btn_img_upload_img = resize_image(img_upload,(200,100))
+btn_img_upload_model = ImageTk.PhotoImage(btn_img_upload_img)
+upload_btn = tk.Button(header_frame, image=btn_img_upload_model,bg= pry_color, borderwidth=0, activebackground=pry_color)
 
 #help button
-img_help = tk.PhotoImage(file=r'src\images\help_img.png')
-img_help_label = tk.Label(image=img_help, background= pry_color)
-help_btn = tk.Button(header_frame, image= img_help, background = pry_color, borderwidth=0, activebackground=pry_color)
+img_help= (Image.open(r'src\images\help_img.png'))
+btn_img_help_img = resize_image(img_help,(200,100))
+btn_img_help_model = ImageTk.PhotoImage(btn_img_help_img)
+help_btn = tk.Button(header_frame, image = btn_img_help_model, bg= pry_color, borderwidth=0, activebackground=pry_color)
+
+# img_help= (Image.open(r'src\images\help_img.png'))
+# btn_img_help_img = resize_image(img_help,(200,100))
+# btn_img_help_model = ImageTk.PhotoImage(btn_img_help_img)
+# help_btn = tk.Button(header_frame, image=btn_img_help_model,bg= pry_color, borderwidth=0, activebackground=pry_color)
 
 #close button
-img_close = tk.PhotoImage(file=r'src\images\cls_btn.png')
-img_close_label = tk.Label(image=img_close, background= pry_color)
-close_btn = tk.Button(header_frame,image=img_close ,bg= pry_color, borderwidth=0, activebackground=pry_color)
+img_close= (Image.open(r'src\images\cls_btn.png'))
+btn_img_close_img = resize_image_predifined(img_close,(35,30))
+btn_img_close_model = ImageTk.PhotoImage(btn_img_close_img)
+close_btn = tk.Button(header_frame, image=btn_img_close_model,bg= pry_color, borderwidth=0, activebackground=pry_color)
 
 #minimize button
-img_minimize = tk.PhotoImage(file=r'src\images\min_btn.png')
-img_minimize_label = tk.Label(image=img_minimize, background= pry_color)
-minimize_btn = tk.Button(header_frame, image=img_minimize ,bg= pry_color, borderwidth=0, activebackground=pry_color)
+img_minimize= (Image.open(r'src\images\min_btn.png'))
+btn_img_minimize_img = resize_image_predifined(img_minimize,(35,30))
+btn_img_minimize_model = ImageTk.PhotoImage(btn_img_minimize_img)
+minimize_btn = tk.Button(header_frame, image=btn_img_minimize_model,bg= pry_color, borderwidth=0, activebackground=pry_color)
 
 #top widget
 header_frame.grid(row=0, column=0, sticky='WENS')
-img_home_btn.grid(row= 0 , column= 0, sticky ='W', ipady=5, ipadx=7)
-upload_btn.grid(row= 0 , column= 1, padx=(0,250), sticky ='E', pady=2)
-help_btn.grid(row= 0 , column= 1, padx=2, sticky ='E', pady=2)
-minimize_btn.grid(row= 0 , column= 2, sticky ='NE', padx=(0,55), pady=5)
-close_btn.grid(row= 0 , column= 2, sticky ='NE', padx=2, pady=5)
+home_btn.grid(row= 0 , column= 0, sticky ='W', ipady=5, ipadx=7)
+upload_btn.grid(row= 0 , column= 1, padx=(0,255), sticky ='E', pady=2)
+help_btn.grid(row= 0 , column= 1, padx=(0,50), sticky ='E', pady=2)
+minimize_btn.grid(row= 0 , column= 2, sticky ='E', padx=(0,45), pady=2)
+close_btn.grid(row= 0 , column= 2, sticky ='E', padx=(2,5), pady=2)
 
+###############
+#main
+###############
 
-#main frame
-main_frame = tk.Frame(root, background= sec_color)
+#main Segmentation
+main_frame = tk.Frame(root, background= bg_container_color)
 main_frame.rowconfigure(0, weight=0)
 main_frame.rowconfigure(1, weight=1)
 main_frame.columnconfigure(0, weight=4)
 main_frame.columnconfigure(1, weight=4)
 main_frame.columnconfigure(2, weight=3)
-original_txt = tk.Label(main_frame, text='Original Image',font= (font, mdx_font, 'bold'), background= sec_color )
-edited_txt = tk.Label(main_frame, text='Edited Image', font= (font, mdx_font, 'bold'), background= sec_color )
-histogram_txt = tk.Label(main_frame, text='Histogram', font= (font, mdx_font, 'bold'), background= sec_color )
-original_img_label= tk.Label(main_frame,text =' imagem original', background='gray')
-edited_img_label = tk.Label(main_frame,text =' imagem editada', background='gray')
-histogram_img_label = tk.Label(main_frame,text =' histograma ', background='gray')
+
+#main Elemnts
+original_txt = tk.Label(main_frame, text='Original Image',font= (font, mdx_font, 'bold'), background= bg_container_color )
+edited_txt = tk.Label(main_frame, text='Edited Image', font= (font, mdx_font, 'bold'), background= bg_container_color )
+histogram_txt = tk.Label(main_frame, text='Histogram', font= (font, mdx_font, 'bold'), background= bg_container_color )
+
+#original image
+original_img_label= tk.Label(main_frame,text =' imagem original', background=sec_color)
+edited_img_label = tk.Label(main_frame,text =' imagem editada', background=sec_color)
+histogram_img_label = tk.Label(main_frame,text =' histograma ', background=sec_color)
 
 
-#main widget
+#main Position
 main_frame.grid(row=1, column=0, sticky='WENS')
+
 original_txt.grid(row=0, column=0, sticky='N', pady=(6,2))
 edited_txt.grid(row=0, column=1, sticky='N', pady=(6,2))
 histogram_txt.grid(row=0, column=2, sticky='N', pady=(6,2))
@@ -108,20 +132,22 @@ original_img_label.grid(row=1, column=0, sticky='WENS', padx=2, pady=5)
 edited_img_label.grid(row=1, column=1, sticky='WENS', padx=2, pady=5)
 histogram_img_label.grid(row=1, column=2, sticky='WENS', padx=2, pady=5)
 
+###############
+#footer frame
+###############
 
-#footer Wdiget
-footer_frame = tk.Frame(root, background=pry_color)
+footer_frame = tk.Frame(root, background=pry_color, height=50)
 footer_frame.rowconfigure(0, weight=1)
 footer_frame.columnconfigure(0, weight=5)
-footer_frame.columnconfigure(1, weight=5)
-footer_frame.columnconfigure(2, weight=4)
+footer_frame.columnconfigure(1, weight=2)
+footer_frame.columnconfigure(2, weight=1)
 
 contrast_frame = tk.Frame(footer_frame, background=pry_color)
 treshold_frame = tk.Frame(footer_frame, background=pry_color)
 save_frame = tk.Frame(footer_frame, background=pry_color)
 
 
-#footer frame
+#footer Wdiget
 footer_frame.grid(row=2, column=0, sticky='WENS')
 contrast_frame.grid(row=0, column=0,sticky='WENS', ipadx=3,padx=2, pady=3)
 treshold_frame.grid(row=0, column=1,sticky='WENS', ipadx=3,padx=2, pady=3)
@@ -131,7 +157,7 @@ save_frame.grid(row=0, column=2, ipadx=3,sticky='WENS',padx=2, pady=3)
 #Contrast Frame
 contrast_frame.columnconfigure(0, weight=1)
 contrast_frame.rowconfigure(0, weight=1)
-contrast_frame.rowconfigure(1, weight=9)
+contrast_frame.rowconfigure(1, weight=1)
 #contrast_frame.rowconfigure(2, weight=1)
 
 #----- scale
@@ -149,7 +175,7 @@ bar_contrast.grid(row=1, column=0,sticky='EW', padx=25, pady=5, ipadx=5, ipady=5
 #treshold Frame
 radio_selected = 0
 
-treshold_frame.columnconfigure(0, weight=7)
+treshold_frame.columnconfigure(0, weight=3)
 treshold_frame.columnconfigure(1, weight=3)
 
 treshold_frame.rowconfigure(0, weight=1)
@@ -166,25 +192,28 @@ def selected_model():
 list_treshold_model = ['OTSU', 'XXX', 'YYYYYYYYYYYYYY']
 clicked_model = tk.StringVar()
 clicked_model.set(list_treshold_model[0])
-dropdow_treshold = tk.OptionMenu(treshold_frame,clicked_model, *list_treshold_model)
-
+# dropdow_treshold = tk.OptionMenu(treshold_frame,clicked_model, *list_treshold_model)
 
 #----- combobox
-combobox_models = ttk.Combobox(treshold_frame, values=list_treshold_model, font= (font, md_font))
+combobox_models = ttk.Combobox(treshold_frame, values=list_treshold_model, font= (font, md_font), state="readonly")
 combobox_models.current(0)
-combobox_models.bind("<<Combobox Selecred>>", selected_model)
+#combobox_models.bind("<<Combobox Selecred>>", selected_model)
 
 
 #----- radioBtn Treshhold
-radio1_btn_treshold =tk.Radiobutton(treshold_frame,text='Automatic', value=0, variable = radio_selected,bg= pry_color,
-                              activebackground= pry_color, font= (font, md_font), command= selected_model)
-radio2_btn_treshold =tk.Radiobutton(treshold_frame,text='Manual', value=1, variable = radio_selected,bg= pry_color,
-                              activebackground= pry_color, font= (font, md_font), command= selected_model)
-value_treshold = tk.Label(treshold_frame, text= 'VALUE: ', bg= pry_color, font= (font, sm_font))
+radio_selected = tk.StringVar(value="automatic")
+radio1_btn_treshold =tk.Radiobutton(treshold_frame,text='Automatic', variable = radio_selected, value= "automatic", bg= pry_color, activebackground= pry_color, font= (font, md_font))
+radio2_btn_treshold =tk.Radiobutton(treshold_frame,text='Manual', variable = radio_selected, value= "manual", bg= pry_color, activebackground= pry_color, font= (font, md_font))
 
-treshold_save = tk.PhotoImage(file=r'src\images\run_btn.png')
-treshold_label = tk.Label(image=treshold_save, background= pry_color)
-treshold_btn = tk.Button(treshold_frame, image=treshold_save,bg= pry_color, borderwidth=0, activebackground=pry_color)
+value_treshold = tk.Label(treshold_frame, text= 'VALUE: ', bg= pry_color, font= (font, sm_font))
+text_box_treshold = tk.Text(treshold_frame, width=4, height=1, font=(font, md_font))
+
+#btn Run Model
+run_model_img= (Image.open(r'src\images\run_btn.png'))
+btn_run_model_img = resize_image(run_model_img,(200,100))
+btn_run_model = ImageTk.PhotoImage(btn_run_model_img)
+treshold_label = tk.Label(image=btn_run_model, background= pry_color)
+treshold_btn = tk.Button(treshold_frame, image=btn_run_model,bg= pry_color, borderwidth=0, activebackground=pry_color)
 
 #treshold Widget 
 
@@ -192,10 +221,11 @@ text_treshold = tk.Label(treshold_frame, text='TRESHOLD',font= (font, mdx_font, 
 text_treshold.grid(row=0, column=0,columnspan=2,sticky='N', padx=3, pady=5)
 #dropdow_treshold.grid(row=1, column=0,sticky='we', padx=5, pady=5)
 combobox_models.grid(row=1, column=0,sticky='N', padx=5, pady=5)
-radio1_btn_treshold.grid(row=2, column=0,sticky='N', padx=5, pady=5)
-radio2_btn_treshold.grid(row=3, column=0,sticky='N', padx=5, pady=5)
+radio1_btn_treshold.grid(row=2, column=0,sticky='N', padx=5, pady=2)
+radio2_btn_treshold.grid(row=3, column=0,sticky='N', padx=5, pady=2)
 value_treshold.grid(row=4, column=0,sticky='N', padx=5, pady=5)
 treshold_btn.grid(row=2, column=1,rowspan=3,sticky='N', padx=3, pady=5)
+text_box_treshold.grid(row=4, column=0,sticky='N', padx=(140,0), pady=2)
 
 #Save Frame
 
@@ -256,9 +286,10 @@ checkbox3_frame = tk.Checkbutton(save_frame,
                               command= display_saves
                               )
 
-
-#save button
-img_save = tk.PhotoImage(file=r'src\images\save_btn.png')
+#Load an image in the script
+img= (Image.open(r'src\images\save_btn.png'))
+img = resize_image(img,(200,100))
+img_save = ImageTk.PhotoImage(img)
 img_save_label = tk.Label(image=img_save, background= pry_color)
 save_btn = tk.Button(save_frame, image=img_save,bg= pry_color, borderwidth=0, activebackground=pry_color)
 
@@ -266,7 +297,7 @@ text_checkbox.grid(row=0, column=0,columnspan=2, sticky='N', padx=2, pady=3)
 checkbox1_frame.grid(row=1, column=0, sticky='w', padx=5, pady=3)
 checkbox2_frame.grid(row=2, column=0, sticky='w', padx=5, pady=3)
 checkbox3_frame.grid(row=3, column=0, sticky='w', padx=5, pady=3)
-save_btn.grid(row=2, column=1, sticky='N', padx=(2,20), pady=3)
+save_btn.grid(row=2, column=1, sticky='N', padx=(2,2), pady=3)
 
 
 
